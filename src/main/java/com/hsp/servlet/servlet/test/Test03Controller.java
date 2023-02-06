25e3580d67a96ed7fd49748ec50c3d7f7fa98774
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-02-06 18:30:48 +0900

    [박현석] -최초파일 추가

---
+++ src/main/java/com/hsp/servlet/servlet/test/Test03Controller.java
@@ -0,0 +1,35 @@
+package com.hsp.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servlet/test03")
+public class Test03Controller extends HttpServlet{
+	
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		Date now = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");  
+		String timeString = formatter.format(now);
+		
+		out.println("<html><head><title>뉴스기사</title></head> <body>");
+		out.println("<h2>[단독] 고양이가 야옹해</h2>");
+		out.println("기사 입력시간 : " + timeString + "<hr>끝</body></html>");
+		
+	}
+	
+
+}
---
+++ src/main/java/com/hsp/servlet/servlet/test/Test06Controller.java
@@ -0,0 +1,47 @@
+package com.hsp.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servlet/test06")
+public class Test06Controller extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/json");
+		
+		PrintWriter out = response.getWriter();
+		
+		// 두개의 수를 전달 받아서, 더하고 빼고, 곱하고, 나눈 결과를 json으로 구성한다.
+		int number1 = Integer.parseInt(request.getParameter("number1"));
+		int number2 = Integer.parseInt(request.getParameter("number2"));
+		
+		int plus = number1 + number2;
+		int minus = number1 - number2;
+		int multi = number1 * number2;
+		int div = number1 / number2;
+		
+		
+		// josn = js규격을 문자열로 표현
+		// {"addition":10, "subtraction":5, "multiplication":25, "division":2}
+		// ""안에 "" => \"
+		out.println("{\"addition\":" + plus 
+				+ ", \"subtraction\":" + minus 
+				+ ", \"multiplication\":" + multi 
+				+ ", \"division\":" + div + "}");
+		
+
+		
+	
+		
+	}
+
+}
