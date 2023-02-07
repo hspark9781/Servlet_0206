e068de65b669ed54ab86f54e12f682247f78aa96
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-02-07 18:42:56 +0900

    [박현석] -post 문제풀이 -jsp개념

--- src/main/java/com/hsp/servlet/servlet/test/Test09Controller.java
+++ src/main/java/com/hsp/servlet/servlet/test/Test09Controller.java
@@ -1,5 +1,33 @@
 package com.hsp.servlet.servlet.test;
 
-public class Test09Controller {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test09")
+public class Test09Controller extends HttpServlet{
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		// 이름과 자기 소개서를 전달받고, html 화면 구성
+		String name = request.getParameter("name");
+		String apply = request.getParameter("apply");
+		
+		out.println("<html><head><title>입사지원</title></head><body>");
+		
+		out.println("<h3>" + name + "님 지원이 완료 되었습니다. </h3> <hr>");
+		out.println("<b>지원내용</b>" + "<br>" + apply);
+		out.println("</body></html>");
+		
+		
+	}
 
 }
--- src/main/java/com/hsp/servlet/servlet/test/Test10Controller.java
+++ src/main/java/com/hsp/servlet/servlet/test/Test10Controller.java
@@ -1,5 +1,58 @@
 package com.hsp.servlet.servlet.test;
 
-public class Test10Controller {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test10")
+public class Test10Controller extends HttpServlet {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	
+		
+		// 아이디와 비밀번호를 전달 받고, 그에 따른 처리를 html로 만든다.
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+
+		out.println("<html><head><title>로그인</title></head><body>");
+
+		// 전달 받은 아이디가 맵의 아이디와 일치여부
+		if(!id.equals(userMap.get("id"))) {
+			out.println("<h2>id가 일치하지 않습니다</h2>");
+			
+		// 전달 받은 비밀번호가 맵의 비밀번호와 일치여부
+		} else if (!password.equals(userMap.get("password"))) {
+			out.println("<h2>비밀번호가 일치하지 않습니다.</h2>");
+			
+		// 아이디와 비밀번호가 맵과 일치할경우
+		} else {
+			out.println("<h2>" + userMap.get("name") + "님 환영합니다." + "</h2>");
+		}
+
+
+		out.println("</body></html>");
+
+
+
+
+	}
+
+	private final Map<String, String> userMap =  new HashMap<String, String>() {
+		{
+			put("id", "hsp9781");
+			put("password", "asdf");
+			put("name", "박현석");
+		}
+	};
 
 }
