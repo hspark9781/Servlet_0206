4ada1c9d55e445439b0e79097ad736a9fa97f21d
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-02-20 16:23:26 +0900

    [박현석] -database/test02/delete 수정2

--- src/main/java/com/hsp/servlet/database/Test02DeleteController.java
+++ src/main/java/com/hsp/servlet/database/Test02DeleteController.java
@@ -11,13 +11,13 @@ import com.hsp.servlet.common.MysqlService;
 @WebServlet("/database/test02/delete")
 public class Test02DeleteController extends HttpServlet {
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
 		MysqlService mysqlService = MysqlService.getInstance();
 		
 		mysqlService.connect();
 		
-		String id = request.getParameter("id");
+		int id = Integer.parseInt(request.getParameter("id"));
 		String query="DELETE FROM `url` WHERE `id` = " + id + ";";
 		
 		int count = mysqlService.update(query);
